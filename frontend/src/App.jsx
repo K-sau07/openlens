943f136bd39ef07ef6dddd8046043fa03f9c3c89
Saurabh Kashyap <kashyap.sau@northeastern.edu>
2026-03-28 16:25:42 -0400

    wired all routes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
+import AnalysisPage from './pages/AnalysisPage'
+import QuizPage from './pages/QuizPage'
+import IssueSelectionPage from './pages/IssueSelectionPage'
+import GuidePage from './pages/GuidePage'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/analysis" element={<AnalysisPage />} />
+      <Route path="/quiz" element={<QuizPage />} />
+      <Route path="/issues" element={<IssueSelectionPage />} />
+      <Route path="/guide" element={<GuidePage />} />
     </Routes>
   )
 }
